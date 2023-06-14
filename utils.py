bbb61524cbe7071285854017d47303f5bed5a3fb
Fadi Zoghlami <zoghlamifadi77@gmail.com>
2023-06-14 18:37:32 +0200

    Updated utils.py

--- utils.py
+++ utils.py
@@ -3,6 +3,8 @@ import torch.nn as nn
 from torch.utils.data import DataLoader
 from tqdm.auto import tqdm
 import time
+import numpy as np
+from itertools import product
 
 
 def compute_accuracy(model, data_loader, device):
@@ -31,6 +33,40 @@ def compute_accuracy(model, data_loader, device):
             correct_pred += (predicted_labels == targets).sum()
 
     return (correct_pred.float()/num_examples) * 100
+
+
+def compute_confusion_matrix(model, data_loader, device):
+
+    all_targets, all_predictions = [], []
+    with torch.no_grad():
+
+        for i, (features, targets) in enumerate(data_loader):
+
+            features = features.to(device)
+            targets = targets
+            logits = model(features)
+            _, predicted_labels = torch.max(logits, 1)
+            all_targets.extend(targets.to('cpu'))
+            all_predictions.extend(predicted_labels.to('cpu'))
+
+    all_predictions = all_predictions
+    all_predictions = np.array(all_predictions)
+    all_targets = np.array(all_targets)
+        
+    class_labels = np.unique(np.concatenate((all_targets, all_predictions)))
+    if class_labels.shape[0] == 1:
+        if class_labels[0] != 0:
+            class_labels = np.array([0, class_labels[0]])
+        else:
+            class_labels = np.array([class_labels[0], 1])
+    n_labels = class_labels.shape[0]
+    lst = []
+    z = list(zip(all_targets, all_predictions))
+    for combi in product(class_labels, repeat=2):
+        lst.append(z.count(combi))
+    mat = np.asarray(lst)[:, None].reshape(n_labels, n_labels)
+    
+    return mat
 
 
 def train_model(model: nn.Module, num_epochs: int, train_loader: DataLoader,
