6eeba6c44498930afb249921b020fde61deb22ef
Fadi Zoghlami <zoghlamifadi77@gmail.com>
2023-06-14 18:22:16 +0200

    Added plot_utils.py

---
+++ plot_utils.py
@@ -0,0 +1,83 @@
+import os
+import numpy as np
+import matplotlib as plt
+
+
+def plot_training_loss(minibatch_loss_list, num_epochs, iter_per_epoch,
+                       results_dir=None, averaging_iterations=100):
+    """
+    Plots the training losses.
+
+    Parameters:
+        minibatch_loss_list (List): list of all calculated losses for each minibatch
+        num:epochs (int): number of epochs
+        results_dir: directory that will be used to save the plotting
+    """
+    plt.figure()
+    ax1 = plt.subplot(1, 1, 1)
+    ax1.plot(range(len(minibatch_loss_list)),
+             (minibatch_loss_list), label='Minibatch Loss')
+
+    if len(minibatch_loss_list) > 1000:
+        ax1.set_ylim([
+            0, np.max(minibatch_loss_list[1000:])*1.5
+            ])
+    ax1.set_xlabel('Iterations')
+    ax1.set_ylabel('Loss')
+
+    ax1.plot(np.convolve(minibatch_loss_list,
+                         np.ones(averaging_iterations,)/averaging_iterations,
+                         mode='valid'),
+             label='Running Average')
+    ax1.legend()
+
+    ###################
+    # Set scond x-axis
+    ax2 = ax1.twiny()
+    newlabel = list(range(num_epochs+1))
+
+    newpos = [e*iter_per_epoch for e in newlabel]
+
+    ax2.set_xticks(newpos[::10])
+    ax2.set_xticklabels(newlabel[::10])
+
+    ax2.xaxis.set_ticks_position('bottom')
+    ax2.xaxis.set_label_position('bottom')
+    ax2.spines['bottom'].set_position(('outward', 45))
+    ax2.set_xlabel('Epochs')
+    ax2.set_xlim(ax1.get_xlim())
+    ###################
+
+    plt.tight_layout()
+
+    if results_dir is not None:
+        image_path = os.path.join(results_dir, 'plot_training_loss.pdf')
+        plt.savefig(image_path)
+
+
+def plot_accuracy(train_acc_list, test_acc_list, results_dir=None):
+    """
+    Plots the training and testing accuracies.
+
+    Parameters:
+        train_acc_list (List): list of all training accuracies
+        test_acc_list (List): list of all testing accuracies
+        results_dir: directory that will be used to save the plotting
+    """
+    num_epochs = len(train_acc_list)
+
+    plt.plot(np.arange(1, num_epochs+1),
+             train_acc_list, label='Training')
+    plt.plot(np.arange(1, num_epochs+1),
+             test_acc_list, label='Validation')
+
+    plt.xlabel('Epoch')
+    plt.ylabel('Accuracy')
+    plt.legend()
+
+    plt.tight_layout()
+
+    if results_dir is not None:
+        image_path = os.path.join(
+            results_dir, 'plot_acc_training_testing.pdf')
+        plt.savefig(image_path)
